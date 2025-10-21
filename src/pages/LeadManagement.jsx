e5ef0e93fa48f568dd8a4c7055eeba56c11667e0
Sowhom638 <sowhom.ghosh.22@aot.edu.in>
2025-10-21 09:45:30 +0530

    Spelling correction

--- src/pages/LeadManagement.jsx
+++ src/pages/LeadManagement.jsx
@@ -101,7 +101,7 @@ function LeadManagement() {
                                             <p className="px-3" style={{ color: `#4C763B` }}><b>Name:</b> {lead?.lead.name}</p>
                                             <p className="px-3"><b>Status:</b> {lead?.lead.status}</p>
                                             <p className="px-3"><b>Sales Agent:</b> {lead?.lead.salesAgent.name}</p>
-                                            <p className="px-3"><b>Lead Sourcet:</b> {lead?.lead.source}</p>
+                                            <p className="px-3"><b>Lead Source:</b> {lead?.lead.source}</p>
                                             <p className="px-3"><b>Time to close:</b> in {remainingDays} days</p>
                                         </div>)
                                         : (
